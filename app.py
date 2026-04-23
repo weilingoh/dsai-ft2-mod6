73621961b63a103c50b45818cda8866c6567f1d0
weilingoh <weilingz@gmail.com>
2026-04-23 05:08:47 +0000

    Third

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ import datetime
 #    load_dotenv()
 
 # for AWS, do not run this because not using .env
-os.environ["GROQ_API_KEY"] = ""
+# os.environ["GROQ_API_KEY"] = ""
 os.environ["GROQ_API_KEY"] = os.environ.get('GROQ_API_KEY')
 
 client = Groq()
